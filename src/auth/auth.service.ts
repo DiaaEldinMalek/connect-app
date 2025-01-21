31f800336fff5528fdc9a25c1f519b2947ab8057
Diaa Sultan <d.sultan@mdbee.ai>
2025-01-21 16:57:23 +0200

    Token validation logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
@@ -19,9 +19,25 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user._id };
+    const payload = { username: user.username, _id: user._id };
+    const access_token = this.jwtService.sign(payload);
+    
     return {
-      access_token: this.jwtService.sign(payload),
+      
+      access_token: access_token,
     };
   }
+  async validate_token(token: string){
+    const body = this.jwtService.decode(token);
+    const existing_user = await this.userService.findById(body._id)
+    if (!existing_user || existing_user.username != body.username) {throw new UnauthorizedException()}
+    
+    const current_time = Math.floor(new Date().getTime() / 1000)
+    
+    if (body.exp <=current_time) {throw new UnauthorizedException("Token expired")} 
+    Logger.log("Valid token")
+  }
+
+
 }
+
